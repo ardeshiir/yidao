4f2e89c6db8464bd2701a3f878b58dc16a6b8053
ardeshirlaghai <ardeshir.laghaei@snapp.doctor>
2025-07-24 13:34:01 +0330

    feat: revalidate

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Hero from '@/components/Home/Hero'
 import ProductsList from '@/components/Home/ProductsList'
 import SecondSection from '@/components/Home/SecondSection'
+export const revalidate = 30
 
 export default async function HomePage() {
 
---
+++ src/components/BeforeLogin/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BeforeLogin: React.FC = () => {
+  return (
+    <div>
+      <p>
+        <b>Welcome</b>
+      </p>
+    </div>
+  )
+}
+
+export default BeforeLogin
